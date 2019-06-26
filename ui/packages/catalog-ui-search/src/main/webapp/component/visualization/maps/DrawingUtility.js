2478332c9a2fcceac00a00f0b6a38bc757d2cd07
Will Will <wbw4sv@virginia.edu>
2019-06-26 08:57:13 -0600

    DDF-04746 Add license headers

--- ui/packages/catalog-ui-search/src/main/webapp/component/visualization/maps/DrawingUtility.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/visualization/maps/DrawingUtility.js
@@ -12,6 +12,20 @@
  * <http://www.gnu.org/licenses/lgpl.html>.
  *
  **/
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
 
 //allows us to get around svg security restrictions in IE11 (see using svg in opengl)
 //make our own image and manually set dimensions because of IE: https://github.com/openlayers/openlayers/issues/3939
